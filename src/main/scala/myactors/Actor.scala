ed7c2865ce28af947e772c1474b3b00ecb2bb4b8
rtorres12 <rafaeltrrs112@gmail.com>
2015-11-20 23:33:15 -0500

    gtk additions

--- src/main/scala/myactors/Actor.scala
+++ src/main/scala/myactors/Actor.scala
@@ -2,17 +2,19 @@ package myactors
 
 
 trait ActorLike {
-  val name : String
-  val inbox : Inbox[Object]
-  def onReceive(msg : ActorMessage) : Unit
+  val name: String
+  val inbox: Inbox[Object]
+
+  def onReceive(msg: ActorMessage): Unit
+
   val thisRef = SomeRef(this)
 }
 
 trait Actor extends ActorLike {
-  val name : String
+  val name: String
   private val innerQueue = new ConcurrentQueue[ActorMessage]()
 
-  val inbox : Inbox[Object] = new Inbox[Object](innerQueue)
+  val inbox: Inbox[Object] = new Inbox[Object](innerQueue)
 
   val logger = new Thread {
     setDaemon(true)
@@ -23,24 +25,25 @@ trait Actor extends ActorLike {
          * If the dequeue returns some value then call onReceive.
          */
         val msg = innerQueue.dequeue()
-        if(msg.isDefined) onReceive(msg.get)
+        if (msg.isDefined) onReceive(msg.get)
       }
     }
   }
 
   logger.start()
 
-  def onReceive(message : ActorMessage) : Unit
+  def onReceive(message: ActorMessage): Unit
 }
 
 trait BetterActor {
-  val name : String
+  val name: String
   private val innerQueue = new java.util.concurrent.ConcurrentLinkedQueue[ActorMessage]()
 
-  val inbox : BetterInbox[Object] = new BetterInbox[Object](innerQueue)
+  val inbox: BetterInbox[Object] = new BetterInbox[Object](innerQueue)
 
   val logger = new Thread {
     setDaemon(true)
+
     override def run() = {
       while (true) {
         /*
@@ -48,12 +51,12 @@ trait BetterActor {
          * If the dequeue returns some value then call onReceive.
          */
         val msg = Option(innerQueue.poll())
-        if(msg.isDefined) onReceive(msg.get)
+        if (msg.isDefined) onReceive(msg.get)
       }
     }
   }
 
   logger.start()
 
-  def onReceive(message : ActorMessage) : Unit
+  def onReceive(message: ActorMessage): Unit
 }
--- src/main/scala/myactors/ConcurrentQueue.scala
+++ src/main/scala/myactors/ConcurrentQueue.scala
@@ -3,7 +3,7 @@ package myactors
 import java.util.concurrent.atomic.AtomicReference
 
 /**
- * A lock less concurrent queue using
+ * A lock less concurrent queue using a do while case loop.
  */
 /*
  * Lock less queue with dummy node as the head to prevent dequeue from accessing the head, and enqueue
@@ -12,21 +12,23 @@ import java.util.concurrent.atomic.AtomicReference
  * MPMC : Multi-producer, multi-consumer queue.
  */
 object ConcurrentQueue {
-  def apply[T]() : ConcurrentQueue[T] = new ConcurrentQueue[T]()
-  def apply[T](e : T*) : ConcurrentQueue[T] = {
-    val q  = new ConcurrentQueue[T]()
-    for(element <- e){
+  def apply[T](): ConcurrentQueue[T] = new ConcurrentQueue[T]()
+
+  def apply[T](e: T*): ConcurrentQueue[T] = {
+    val q = new ConcurrentQueue[T]()
+    for (element <- e) {
       q.enqueue(element)
     }
     q
   }
 }
-class ConcurrentQueue[T]{
-  val dummy : Node[T] = new Node[T](next = null)
-  val head : AtomicReference[Node[T]] = new AtomicReference[Node[T]](dummy)
-  val tail : AtomicReference[Node[T]] =  new AtomicReference[Node[T]](dummy)
 
-  def dequeue() : Option[T] = {
+class ConcurrentQueue[T] {
+  val dummy: Node[T] = new Node[T](next = null)
+  val head: AtomicReference[Node[T]] = new AtomicReference[Node[T]](dummy)
+  val tail: AtomicReference[Node[T]] = new AtomicReference[Node[T]](dummy)
+
+  def dequeue(): Option[T] = {
     /*
      *                                          Bad Conditions
      * The next node is not null but the CAS fails then retry, because the operation was interleaved
@@ -43,8 +45,8 @@ class ConcurrentQueue[T]{
      *
      *
      * */
-    var headNode : Node[T] = null
-    var valueNode : Node[T] = null
+    var headNode: Node[T] = null
+    var valueNode: Node[T] = null
 
     do {
       //Get the head node and it's next.
@@ -57,36 +59,37 @@ class ConcurrentQueue[T]{
      * Do a null check on head's next node to prevent a NullPointerException.
      *
      */
+    valueNode match {
+      case null => None
+      case _ =>
+        /*
+         * If valueNode's value is null then don't throw exception, just return None as the value.
+         *
+         * */
+        val value = Option(valueNode.value)
+        /*
+         * If the valueNode is not null then set it's value to null to keep it dummy.
+         *
+         * */
+        if (valueNode != null) valueNode.value = None
 
-    try{
-
-      /*
-       * If valueNode's value is null then don't throw exception, just return None as the value.
-       * */
-      val value = Option(valueNode.value)
-      /*
-       * If the valueNode is not null then set it's value to null to keep it dummy.
-       * */
-      if(valueNode != null) valueNode.value = None
-
-      /*
-       * Return an option of the value.
-       * Catch a NullPointerException thrown by valueNode.value and return None.
-       */
-      value.get
-    } catch {
-      case npe : NullPointerException => None
+        /*
+         * Return an option of the value.
+         * Catch a NullPointerException thrown by valueNode.value and return None.
+         */
+        value.get
     }
   }
 
-  def enqueue(element : T) : Unit = {
+  def enqueue(element: T): Unit = {
     val newNode = new Node[T](Some(element), null)
     val prevTailNode = tail.getAndSet(newNode)
     prevTailNode.next = newNode
   }
 
-  def isEmpty : Boolean = {
+  def isEmpty: Boolean = {
     println(head.get())
     head.get().next != null
   }
+
 }
